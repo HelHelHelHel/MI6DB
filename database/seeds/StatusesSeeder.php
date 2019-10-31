c3c5fb7360384baa710a9fa4fcc9f0c539aba7e4
Hel <helenahrubesova80@gmail.com>
2019-10-31 11:36:22 +0100

    status seeding

---
+++ database/seeds/StatusesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Status;
+class StatusesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Status::insert([
+            ['status' =>  'Active'],
+            ['status'=> 'Alive'],
+            ['status'=> 'Arrested'],
+            ['status'=> 'Deceased'],
+            ['status'=> 'In Custody'],
+            ['status'=> 'Incarcerated'],
+            ['status'=> 'Retired'],
+            ['status'=> 'Unknown']
+        ]);
+    }
+}
